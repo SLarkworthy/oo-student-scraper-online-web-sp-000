5ef37d13cfc7a5b05641cfa24c257f8e3b303c4e
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-23 21:39:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,6 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each {|key, value| self.send(("#{key}="), value)}
-    end
   end
 
   def add_student_attributes(attributes_hash)
