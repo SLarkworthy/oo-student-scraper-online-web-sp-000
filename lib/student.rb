ec74b5869fe619b11bf7565786853d69daae5188
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-23 21:44:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student_hash|
-      Student.new(student_hash)
+      self.new(student_hash)
     end
   end
 
